d2451da3694f51c4122384081d7e6a9e7685b32f
jhwangwoo <125618964+jhwangwoo@users.noreply.github.com>
2023-04-25 18:15:17 +0900

    게임이펙트
    ㅇ

---
+++ javascript/game/js/music.js
@@ -0,0 +1,54 @@
+const allMusic = [
+    {
+        name : "1. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view01",
+        audio: "music_audio01"
+    },{
+        name : "2. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view02",
+        audio: "music_audio02"
+    },{
+        name : "3. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view03",
+        audio: "music_audio03"
+    },{
+        name : "4. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view04",
+        audio: "music_audio04"
+    },{
+        name : "5. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view05",
+        audio: "music_audio05"
+    },{
+        name : "6. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view06",
+        audio: "music_audio06"
+    },{
+        name : "7. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view07",
+        audio: "music_audio07"
+    },{
+        name : "8. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view08",
+        audio: "music_audio08"
+    },{
+        name : "9. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view09",
+        audio: "music_audio09"
+    },{
+        name : "10. 저리가라",
+        artist : "parrick parrick",
+        img: "musoc_view10",
+        audio: "music_audio10"
+    }
+    
+]
